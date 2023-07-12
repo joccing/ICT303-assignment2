69c4f60a4e552ee231d2432ee155ac4c5d6b6efb
Tay Joc Cing <joccing@gmail.com>
2023-07-12 22:17:01 +0800

    Single zip file

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ def config_data(base_path, zipfiles):
         full_path = os.path.join(base_path, filename)
 
         # Derive the name of the expected output directory
-        expected_output = os.path.join(base_path, filename.split('.')[0]) 
+        expected_output = os.path.join(base_path, filename.split('.')[0])
         if filename.endswith(".zip") and not filename.endswith(".csv.zip"):
             expected_output += "/"
 
@@ -52,6 +52,10 @@ def config_data(base_path, zipfiles):
                                 pbar.update()
             else:
                 print(f'{filename} is not a valid zip file')
+                return 0
         else:
             print(f'{filename} does not exist')
+            return 0
+            
     print('\nFinished!')
+    return 1
