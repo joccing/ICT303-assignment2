093b0375d4cf12cee4740ac7d05258ae1d88763f
Tay Joc Cing <joccing@gmail.com>
2023-07-14 10:43:24 +0800

    Refactored

--- config.py
+++ config.py
@@ -33,48 +33,48 @@ def config_data(base_path, zipfiles, expected_names=[]):
 
     if expected_names and check_all(base_path, expected_names):
         print(f'Files expected already exist. Skipping extraction...')
-        return -1 # signify that all seems fine even though none are extracted
+        total = -1 # signify that all seems fine even though none are extracted
+    else:
+        for filename in zipfiles:
+            full_path = os.path.join(base_path, filename)
 
-    for filename in zipfiles:
-        full_path = os.path.join(base_path, filename)
+            # Derive the name of the expected output directory
+            expected_output = os.path.join(base_path, filename.split('.')[0])
+            if filename.endswith(".zip") and not filename.endswith(".csv.zip"):
+                expected_output += "/"
 
-        # Derive the name of the expected output directory
-        expected_output = os.path.join(base_path, filename.split('.')[0])
-        if filename.endswith(".zip") and not filename.endswith(".csv.zip"):
-            expected_output += "/"
+            # If the expected directory already exists, skip extraction
+            if os.path.isdir(expected_output):
+                print(f'Output directory for {filename} already exists. Skipping extraction...')
+                continue
 
-        # If the expected directory already exists, skip extraction
-        if os.path.isdir(expected_output):
-            print(f'Output directory for {filename} already exists. Skipping extraction...')
-            continue
-
-        # Check if the file exists
-        if os.path.exists(full_path):
-            print(f'{filename} exists')
-            
-            # Check if it's a valid zip file
-            if zipfile.is_zipfile(full_path):
-                print(f'{filename} is a valid zip file')
+            # Check if the file exists
+            if os.path.exists(full_path):
+                print(f'{filename} exists')
                 
-                # automatically unzip valid zip files
-                with zipfile.ZipFile(get_file_path(base_path,filename), 'r') as z:
-                  # Create a progress bar
-                    with tqdm(total=len(z.namelist())) as pbar:
-                            # print('\n')
-                            for file in z.namelist():
-                                # Skip any "__MACOSX" files or directories
-                                if "__MACOSX" not in file:
-                                    # print(f'Extracting: {file}')
-                                    z.extract(member=file, path=base_path)
-                                    # Update the progress bar
-                                    pbar.update()
-                                    total += 1
+                # Check if it's a valid zip file
+                if zipfile.is_zipfile(full_path):
+                    print(f'{filename} is a valid zip file')
+                    
+                    # automatically unzip valid zip files
+                    with zipfile.ZipFile(get_file_path(base_path,filename), 'r') as z:
+                    # Create a progress bar
+                        with tqdm(total=len(z.namelist())) as pbar:
+                                # print('\n')
+                                for file in z.namelist():
+                                    # Skip any "__MACOSX" files or directories
+                                    if "__MACOSX" not in file:
+                                        # print(f'Extracting: {file}')
+                                        z.extract(member=file, path=base_path)
+                                        # Update the progress bar
+                                        pbar.update()
+                                        total += 1
+                else:
+                    print(f'{filename} is not a valid zip file')
+                    return total
             else:
-                print(f'{filename} is not a valid zip file')
+                print(f'{filename} does not exist')
                 return total
-        else:
-            print(f'{filename} does not exist')
-            return total
 
     print('\nFinished!')
     return total
